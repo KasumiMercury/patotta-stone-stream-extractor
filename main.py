ff636282fd2d422bc5c4f2ec5639b8881f3a2619
KasumiMercury <naoki_shira@outlook.jp>
2024-03-29 01:07:19 +0900

    Get param from request

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ import functions_framework
 
 @functions_framework.http
 def function(request):
+    vid = request.args.get('vid')
+    if vid is not None:
+        return f"Hello {vid}!"
+    
     return "Hello World!"
+
