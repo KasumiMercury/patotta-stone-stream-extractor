fc9480798cc2588149b9e7e66f0df90fc103079b
KasumiMercury <naoki_shira@outlook.jp>
2024-03-29 01:19:20 +0900

    🚧 Error handling

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import functions_framework
 @functions_framework.http
 def function(request):
     vid = request.args.get('vid')
-    if vid is not None:
-        return f"Hello {vid}!"
+    # If vid is not provided, report an error
+    if vid is None:
+        raise RuntimeError("No vid provided")
     
     return "Hello World!"
 
